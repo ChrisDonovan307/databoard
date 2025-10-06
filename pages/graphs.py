8fd6fbceb60a4180f3f58ead8e29ace878ff8dc9
Christopher Donovan <christopher.donovan@uvm.edu>
2025-10-05 22:04:22 -0400

    adding test table page

--- pages/graphs.py
+++ pages/graphs.py
@@ -53,34 +53,35 @@ slider = dcc.Slider(min=0, max=10, step=1)
 # ])
 
 layout = dbc.Container([
-    dbc.Row(html.H1(children='Bootstrap Dash App', style={'textAlign': 'center'})),
-    html.Span(children=[
-        f"Prepared: {datetime.datetime.now().strftime('%Y-%m-%d')}",
-        html.Br(),
-        " by ",
-        html.B("John Doe, "),
-        html.Br(),
-        html.I("Mediocre Guy")
-    ]),
-    html.H2(children='Histogram', style={'textAlign': 'center'}),
+    # Header
+    html.Div([
+        html.H1('Iris Dataset Visualizations', style={'textAlign': 'center', 'color': '#2c3e50'}),
+        html.P(f"Prepared: {datetime.datetime.now().strftime('%Y-%m-%d')}",
+               style={'textAlign': 'center', 'color': '#7f8c8d'}),
+        html.Hr()
+    ], style={'marginBottom': '30px'}),
+
+    # Main Content
+    html.H2('Petal Width Distribution', style={'textAlign': 'center'}),
     dcc.Graph(id='test', figure=hist_graph),
-    html.H2(children='Bar', style={'textAlign': 'center'}),
+
+    html.H2('Mean Petal Width by Species', style={'textAlign': 'center'}),
     html.Div(
         dcc.Graph(id='test2', figure=bar_graph, style={'width': '60%'}),
         style={'display': 'flex','justifyContent': 'center'}
     ),
 
-    # Test bootstrap columns
-    # Still not appearing in columns like we want - they are just even
-    html.H2(children='Bootstrap Rows', style={'textAlign': 'center'}),
-    dbc.Row(
-        [
-            dbc.Col(dcc.Graph(id='test3', figure=hist_graph), width=2),
-            dbc.Col(dcc.Graph(id='test4', figure=bar_graph), width=10)
-        ]
-        # style={'display': 'flex'}
-        # Remove justify content center to get this to work
-    )
+    html.H2('Comparative View', style={'textAlign': 'center'}),
+    dbc.Row([
+        dbc.Col(dcc.Graph(id='test3', figure=hist_graph), width=2),
+        dbc.Col(dcc.Graph(id='test4', figure=bar_graph), width=10)
+    ]),
+
+    # Footer
+    html.Div([
+        html.Hr(),
+        html.P('© 2025 Databoard', style={'textAlign': 'center', 'color': '#7f8c8d', 'fontSize': '14px'})
+    ], style={'marginTop': '50px'})
 ])
 # ], fluid=True) # not sure if this matters
 # Id doesn't matter if we aren't calling back apparently
--- pages/home.py
+++ pages/home.py
@@ -1,13 +1,28 @@
-from dash import Dash, html, dcc
+from dash import html
 import dash
 
 dash.register_page(__name__, path='/home')
 
-layout = html.Div(children=[
-    html.H1(children='This is our Home page'),
+layout = html.Div([
+    # Header
+    html.Div([
+        html.H1('Welcome to Databoard', style={'textAlign': 'center', 'color': '#2c3e50'}),
+        html.Hr()
+    ], style={'marginBottom': '30px'}),
 
-    html.Div(children='''
-        This is our Home page content.
-    '''),
+    # Main Content
+    html.Div([
+        html.H2('Data Visualization Dashboard'),
+        html.P('Explore interactive visualizations and data tables using the navigation above.'),
+        html.Ul([
+            html.Li('Graphs - View iris dataset visualizations'),
+            html.Li('Tables - Browse interactive data tables')
+        ])
+    ], style={'maxWidth': '800px', 'margin': '0 auto', 'padding': '20px'}),
 
+    # Footer
+    html.Div([
+        html.Hr(),
+        html.P('© 2025 Databoard', style={'textAlign': 'center', 'color': '#7f8c8d', 'fontSize': '14px'})
+    ], style={'marginTop': '50px'})
 ])
