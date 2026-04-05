019323c7af3bd82109fad8304887fcf2771b0b09
cdonovan <ctudonovan@gmail.com>
2026-04-05 18:48:00 -0400

    favicon

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import favicon from '$lib/assets/favicon.svg';
+	// import favicon from '$lib/assets/favicon.svg';
 	import "../app.css"
 	import { base } from '$app/paths';
   import Navbar from './Navbar.svelte'
@@ -15,8 +15,8 @@
 </script>
 
 <svelte:head>
-	<link rel="icon" href={favicon} />
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css" />
+	<link rel="icon" href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><rect width='100' height='100' rx='15' fill='%23555'/><text x='50' y='72' font-size='60' font-family='sans-serif' font-weight='bold' fill='white' text-anchor='middle'>DB</text></svg>">
 </svelte:head>
 
 <Navbar />
